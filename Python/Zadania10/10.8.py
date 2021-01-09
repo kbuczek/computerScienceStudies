559b1e9435892dba82f9158ec8bdd7395532067d
kbuczek <chris.buczek.1998@gmail.com>
2021-01-09 09:54:52 +0100

    Add files via upload

---
+++ Python/Zadania10/10.8.py
@@ -0,0 +1,48 @@
+import random
+
+class RandomQueue:
+
+    def __init__(self):
+        self.items = []
+
+    def __str__(self):
+        return str(self.items)
+
+    def __len__(self):
+        return len(self.items)
+
+    def insert(self, item):
+        self.items.append(item)
+
+    def remove(self):   # zwraca losowy element
+        if self.is_empty():
+            raise ValueError("Kolejka jest pusta!")
+        i = random.randint(0, len(self.items) - 1)
+        temp = self.items[i]
+        self.items[i] = self.items[-1]
+        self.items[-1] = temp
+        return self.items.pop()
+
+    def is_empty(self):
+        return not self.items
+
+    def is_full(self):
+        return False
+
+    def clear(self):     # czyszczenie listy
+        self.items.clear()
+
+randomQueue = RandomQueue()
+for i in range(5):
+    randomQueue.insert(i)
+print(randomQueue)
+
+for i in range(len(randomQueue)):     
+    randomQueue.remove()
+    print(randomQueue)
+
+randomQueue.insert(1)
+randomQueue.clear()
+print(randomQueue)
+
+
